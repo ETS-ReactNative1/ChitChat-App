57b628cb3e2034e979908b6fffd4aa457384f0a9
vaishnav ambre <vaishnavambre31@gmail.com>
2021-10-29 16:40:48 +0530

    firebase setup

--- src/screens/User/Chat/index.js
+++ src/screens/User/Chat/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react'
 import { FlatList } from 'react-native'
-import { io } from "socket.io-client";
 
 import Container from '../../../components/Container'
 import AppHeader from '../../../components/AppHeader'
