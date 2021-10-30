27839aa5b1fff9796239384a199d4fcacc514691
vaishnav ambre <vaishnavambre31@gmail.com>
2021-10-30 20:48:06 +0530

    untracked files

--- src/config/index.js
+++
@@ -1,6 +0,0 @@
-// google login creds
-export const webClientId = '<< Enter your web client id here >>'
-
-// twitter login creds
-export const twitterApikey = '<< Enter your twitter api key here >>'
-export const twitterkeySecret = '<< Enter your twitter secret key here >>'
